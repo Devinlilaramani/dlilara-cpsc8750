fdf7da00bb4d165b43c71180dfd23f61e9c2a94f
Devin Sunil Lilaramani <Devin.Lilaramani@ibm.com>
2022-10-31 00:51:38 -0400

    API Layer
    Added API Layer

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+const fetch = require('node-fetch');
 // use the express library
 const express = require('express');
 // Define the port we will listen on
@@ -30,6 +31,52 @@ app.get('/', (req, res) => {
         //diff: Math.round((Date.now() - new Date(parseInt(cookie['visited']))) / 1000)
     });
 });
+
+app.get("/trivia", async (req, res) => {
+    // fetch the data
+    const response = await fetch("https://opentdb.com/api.php?amount=1&type=multiple");
+  
+    // fail if bad response
+    if (!response.ok) {
+      res.status(500);
+      res.send(`Open Trivia Database failed with HTTP code ${response.status}`);
+      return;
+    }
+  
+    // interpret the body as json
+    const content = await response.json();
+  
+    // fail if db failed
+    if (content.response_code !== 0) {
+      res.status(500);
+      res.send(`Open Trivia Database failed with internal response code ${data.response_code}`);
+      return;
+    }
+  
+    // respond to the browser
+
+    correctAnswer = content.results[0]['correct_answer'];
+    answers = content.results[0]['incorrect_answers'];
+    answers.push(correctAnswer)
+    let shuffledAnswers = answers.sort(function() {
+        return Math.random() - 0.5;
+    });
+
+    const answerLinks = shuffledAnswers.map(answer => {
+    return `<a style='color:green' href="javascript:alert('${
+    answer === correctAnswer ? 'Correct!' : 'Incorrect, Please Try Again!'
+    }')">${answer}</a>
+  }`
+})
+    // TODO: make proper html
+    res.render('trivia', {
+        question: content.results[0]['question'],
+        category: content.results[0]['category'],
+        difficulty: content.results[0]['difficulty'],
+        answers: answerLinks
+    })
+    //res.send(JSON.stringify(content, 2));
+  });
 
 // Start listening for network connections
 app.listen(port);
